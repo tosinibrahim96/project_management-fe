5d12abe7a8b9ead23411523bc186945ca1a165e5
tosinibrahim96 <tosinibrahim96@gmail.com>
2020-07-09 12:11:15 +0100

    Fix: mobile responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,6 @@ import { Loader } from "../components";
 const Home = () => {
   const data = useGetDashboardInfo();
 
-
-
   if (data.loading) {
     return <Loader />;
   }
@@ -36,58 +34,64 @@ const Home = () => {
       },
     } = data;
 
-
-  return (
-    <Container>
-      <Row className="mt-4 mx-auto">
-        <Col md={12}>
-          <h1 className="text-center">Hi, {name}</h1>
-        </Col>
-        <Col md={12}>
-          <h3 className="text-center">Here is an overview of your projects</h3>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col xs={12} md={6} lg={4} className="mt-4">
-          <Card border="primary" className="text-center">
-            <Card.Header> Total number of projects</Card.Header>
-            <Card.Body>
-              <Card.Title style={{ fontSize: "2rem" }}>{projects}</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} md={6} lg={4} className="mt-4">
-          <Card border="secondary" className="text-center">
-            <Card.Header className="text-center">
-              {" "}
-              Total number of tasks
-            </Card.Header>
-            <Card.Body>
-              <Card.Title style={{ fontSize: "2rem" }}>{tasks}</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} md={6} lg={4} className="mt-4">
-          <Card border="success" className="text-center">
-            <Card.Header>Total amount received</Card.Header>
-            <Card.Body>
-              <Card.Title style={{ fontSize: "1.5rem" }}>
-                {`N${total_amount_received} / N${total_amount_expected}`}
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Link md={12} className="offset-md-5" to={"/projects"}>
-          <Button className="btn btn-primary" size="lg">
-            View all projects
-          </Button>
-        </Link>
-      </Row>
-    </Container>
-  );
-}
+    return (
+      <Container>
+        <Row className="mt-4 mx-auto">
+          <Col md={12}>
+            <h1 className="text-center">Hi, {name}</h1>
+          </Col>
+          <Col md={12}>
+            <h3 className="text-center">
+              Here is an overview of your projects
+            </h3>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col xs={12} md={6} lg={4} className="mt-4">
+            <Card border="primary" className="text-center">
+              <Card.Header> Total number of projects</Card.Header>
+              <Card.Body>
+                <Card.Title style={{ fontSize: "2rem" }}>{projects}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={6} lg={4} className="mt-4">
+            <Card border="secondary" className="text-center">
+              <Card.Header className="text-center">
+                {" "}
+                Total number of tasks
+              </Card.Header>
+              <Card.Body>
+                <Card.Title style={{ fontSize: "2rem" }}>{tasks}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={6} lg={4} className="mt-4">
+            <Card border="success" className="text-center">
+              <Card.Header>Total amount received</Card.Header>
+              <Card.Body>
+                <Card.Title style={{ fontSize: "1.5rem" }}>
+                  {`N${total_amount_received} / N${total_amount_expected}`}
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col
+            style={{ width: "50%", margin: "0 auto" }}
+            md={2}
+          >
+            <Link to={"/projects"}>
+              <Button className="btn btn-primary" size="lg">
+                View projects
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 };
 
 export default Home;
